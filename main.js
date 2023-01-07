4072d7a5ee6c2783b6afc56149deaf0ce75ff06a
Yogendra Raj <yogendraraj0206@gmail.com>
2023-01-07 15:16:34 +0530

    another route commit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 app.get('/' , (req,res)=>{
     res.status(200).json({message : "Happy to host my nodejs app" , 'hosted' : true});
 })
-app.get('/message' , ()=>{
+app.get('/message' , (req,res)=>{
     res.send("Hello World!");
 })
 app.listen(port , ()=>console.log(`server is up at ${port}`));
