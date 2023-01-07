8631e52df1ab52dc54543adf3e290b9bb57eb5ba
Yogendra Raj <yogendraraj0206@gmail.com>
2023-01-07 15:05:59 +0530

    next commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/' , (req,res)=>{
+    res.status(200).json({message : "Happy to host my nodejs app" , 'hosted' : true});
+})
+app.get('/message' , ()=>{
+    res.send("Hello World!");
+})
+app.listen(port , ()=>console.log(`server is up at ${port}`));
